ca63d4e46651bd60200b4ed06eed8ab08a30cdb7
heitorrbarros <heitorrbarros@gmail.com>
2019-11-21 16:00:19 -0300

    command lines and create fix

--- checkout/src/sales/handlers/create.js
+++ checkout/src/sales/handlers/create.js
@@ -2,33 +2,44 @@ const dynamodb = require('aws-sdk/clients/dynamodb');
 const uuid = require('uuid/v4');
 const docClient = new dynamodb.DocumentClient();
 const tableName = process.env.SAMPLE_TABLE;
-
 exports.createSaleHandler = async(event) => {
-    if (event.httpMethod !== 'POST') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    try {
+        if (event.httpMethod !== 'POST') {
+            throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+        }
+
+        console.info('received:', event);
+
+        const body = JSON.parse(event.body);
+        body.id = uuid();
+        const Item = Object.assign({}, body);
+
+        var params = {
+            TableName: tableName,
+            Item
+        };
+
+        const { requestContext } = event;
+        const Location = !!requestContext ? `https://${requestContext.domainName}${requestContext.path}/${body.id}` : body.id;
+        const result = await docClient.put(params).promise();
+        const response = {
+            statusCode: 201,
+            headers: {
+                Location,
+                "Access-Control-Allow-Origin": "*"
+            }
+        };
+
+        return response;
+    } catch (error) {
+        console.log('error %j', error);
+        const response = {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify(error)
+        }
+        return response;
     }
-
-    console.info('received:', event);
-
-    const body = JSON.parse(event.body);
-    body.id = body.id || uuid();
-    const Item = Object.assign({}, body);
-
-    var params = {
-        TableName: tableName,
-        Item
-    };
-
-    const Location = !!a.requestContext ? `${a.requestContext.domainName}${a.requestContext.path}/${body.id}` : body.id;
-    const result = await docClient.put(params).promise();
-    const response = {
-        statusCode: 201,
-        Location,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        },
-        body: JSON.stringify(body)
-    };
-
-    return response;
 }
